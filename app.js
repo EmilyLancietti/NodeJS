5bcc047f575de25fdbbf1c394203d65d308cf93a
Emily Lancietti <emily.lancietti@gmail.com>
2018-08-20 10:14:26 +0200

    User authentication

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ mongoose.Promise = require('bluebird');
 const Dishes = require('./models/dishes');
 const Leaders = require('./models/leaders');
 const Promotions = require('./models/promotions');
+const Users = require('./models/user');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
@@ -47,51 +48,31 @@ app.use(session({
     store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
     console.log(req.session);
 
     if (!req.session.user) {
-        var authHeader = req.headers.authorization;
-
-        if (!authHeader) {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
-
-        var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-        var username = auth[0];
-        var password = auth[1];
-
-        if (username === 'admin' && password === 'password') {
-            req.session.user = 'admin';
-
-            next();
-        } else {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
+        var err = new Error('You are not authenticated!');
+        err.status = 403;
+        return next(err);
     } else {
-        if (req.session.user === 'admin') {
+        if (req.session.user === 'authenticated') {
             next();
         } else {
             var err = new Error('You are not authenticated!');
-            err.status = 401;
+            err.status = 403;
             return next(err);
         }
     }
-
-}
+};
 
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promotionRouter);
 app.use('/leaders', leaderRouter);
